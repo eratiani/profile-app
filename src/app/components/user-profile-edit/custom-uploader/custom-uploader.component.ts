91675c5e14d643055064e19f7f359ca3ca2d0762
eratiani <eratiani@gmail.com>
2024-11-11 18:08:44 +0400

    refactor code

--- src/app/components/user-profile-edit/custom-uploader/custom-uploader.component.ts
+++ src/app/components/user-profile-edit/custom-uploader/custom-uploader.component.ts
@@ -24,7 +24,6 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 export class CustomUploaderComponent implements ControlValueAccessor {
   uploader = viewChild('uploader', { read: ElementRef<any> });
   fileUrls = signal<string[]>([]);
-  filesArr = signal<any>([]);
   formData: any;
   onChange = (value: any) => {};
   onTouched = () => {};
@@ -51,7 +50,6 @@ export class CustomUploaderComponent implements ControlValueAccessor {
         var baseStringResult = reader.result as string;
         const imageUrl = baseStringResult;
         this.fileUrls.set([imageUrl]);
-        this.filesArr.set(filefileInputElement?.files?.[0]);
         this.onChange(imageUrl);
         filefileInputElement.value = '';
       };
